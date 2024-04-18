9b53247e3c44c060d9bc5d328dc1ab6ea61fe7d1
jyw28 <roy03280328@gmail.com>
2024-04-18 20:06:24 +0900

    [Feature] :: Set textSelectionTheme

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:dear_app/Auth/Onboarding/ui/onboardingView.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(OnboardingView());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -12,18 +12,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            backgroundColor: Colors.white,
-            body: Center(
-              child: Text(
-                "DEAR.",
-                style: TextStyle(
-                  fontFamily: 'Assistant',
-                  fontWeight: FontWeight.w800,
-                  fontSize: 55,
-                  color: Color(0xff0E2764)
-                ),
-              ),
-            )));
+      theme: ThemeData(
+          textSelectionTheme: TextSelectionThemeData(
+              cursorColor: Color(0xff0E2764),
+              selectionColor: Color(0xff0E2764).withOpacity(0.2),
+              selectionHandleColor: Color(0xff0E2764))),
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Text(
+            "DEAR.",
+            style: TextStyle(
+                fontFamily: 'Assistant',
+                fontWeight: FontWeight.w800,
+                fontSize: 55,
+                color: Color(0xff0E2764)),
+          ),
+        ),
+      ),
+    );
   }
 }
