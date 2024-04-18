5413fbc6a0b06e4a0c29eba910c878463790ad9e
jyw28 <roy03280328@gmail.com>
2024-04-18 16:40:20 +0900

    [Feature] :: Create Onborading View

---
+++ lib/Auth/Onboarding/ui/onboardingView.dart
@@ -0,0 +1,101 @@
+import 'package:dear_app/Auth/Onboarding/component/speechBubble.dart';
+import 'package:flutter/material.dart';
+
+class onboardingView extends StatelessWidget {
+  const onboardingView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 400),
+              const Padding(
+                padding: EdgeInsets.only(bottom: 3),
+                child: Text(
+                  "사소한 질문도 소중히",
+                  style: TextStyle(
+                    fontSize: 23,
+                    fontFamily: "Pretendard",
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const Text(
+                "DEAR.",
+                style: TextStyle(
+                  fontSize: 50,
+                  fontFamily: "Assistant",
+                  fontWeight: FontWeight.w800,
+                  color: Color(0xff0E2764),
+                ),
+              ),
+              SizedBox(
+                height: 13,
+              ),
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  // Padding(
+                  //   padding: EdgeInsets.only(top: 10),
+                  //   child: speechBubble(),
+                  // ),
+                  speechBubble(),
+                  const Text(
+                    "회원가입하고 숨은 꿀팁 찾기",
+                    style: TextStyle(fontSize: 10, height: 1.2),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  fixedSize: const Size(342, 56),
+                  foregroundColor: Colors.white,
+                  backgroundColor: const Color(0xff0E2764),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+                child: const Text(
+                  "시작하기",
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: "Pretendard",
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              const SizedBox(height: 9),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  fixedSize: const Size(342, 56),
+                  foregroundColor: Colors.white,
+                  backgroundColor: const Color(0xffD5DCEC),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+                child: const Text(
+                  "로그인",
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: "Pretendard",
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              const SizedBox(height: 8),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
