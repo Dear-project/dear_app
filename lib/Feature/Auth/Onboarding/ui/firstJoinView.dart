f50e3a2b319ceb756f91861cd1f54b50fda74dc9
신민호 <68156836+alsh0807@users.noreply.github.com>
2024-05-08 14:06:33 +0900

    [Feature] :: FirstJoinView 애니메이션 완성

--- lib/Feature/Auth/Onboarding/ui/firstJoinView.dart
+++ lib/Feature/Auth/Onboarding/ui/firstJoinView.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:dear_app/Shared/component/dearLogo.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -11,91 +12,155 @@ class FirstJoinView extends StatefulWidget {
 }
 
 class _FirstJoinViewState extends State<FirstJoinView>
-    with TickerProviderStateMixin {
-  late AnimationController _offSetController;
-  late Animation<Offset> _offset;
+    with SingleTickerProviderStateMixin {
 
-
+  late AnimationController controller;
+  late Animation<double> animation;
+  double offsetY = -124;
 
   @override
   void initState() {
     super.initState();
-    _offSetController = AnimationController(
-      duration: const Duration(milliseconds: 800),
+
+    controller = AnimationController(
+      duration: Duration(milliseconds: 500),
       vsync: this,
 
+    )
+    ..forward()
+      ..addListener(() async {
+        if (controller.isCompleted) {
 
-    );
 
-    _offset =
-        Tween<Offset>(begin: Offset(0.37, 8.9), end: Offset(0.37, 9.7)).animate(
-            _offSetController);
+          controller.stop();
 
-    _offSetController.repeat();
+          await Future.delayed(Duration(milliseconds: 500));
 
+          controller.reset();
+
+          if (offsetY < 62) {
+            offsetY += 62;
+          }
+          else {
+            offsetY = -124;
+          }
+
+          controller.forward();
+
+
+        }
+      });
+
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
   }
 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.white,
         body: Center(
-      child: Stack(
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              DearLogo(
-                size: 50,
-                color: Color(0xffD1D1D1),
-              ),
-              SizedBox(height: 40),
-              Container(
-                padding: EdgeInsets.only(bottom: 80),
-                height: 240,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  width: 340,
+                  height: 56,
+                  decoration: BoxDecoration(
+                      color: Color(0xffEBEFFF),
+                      border: Border.all(color: Color(0xffE6E6E6)),
+                      borderRadius: BorderRadius.circular(13)),
+                ),
+                AnimatedBuilder(
+                  animation: controller,
+                  builder: (context, child) =>
+                      Transform.translate(
+                        offset: Offset(0, controller.value * 62 + offsetY),
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              Text(
+                                "학생이 학생에게",
+                                style: TextStyle(
+                                    fontFamily: "Pretendard",
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.w800,
+                                    color: Color(0xff0E2764)),
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Text(
+                                "학생이 교수에게",
+                                style: TextStyle(
+                                    fontFamily: "Pretendard",
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.w800,
+                                    color: Color(0xff0E2764)),
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Text(
+                                "학생이 AI에게",
+                                style: TextStyle(
+                                    fontFamily: "Pretendard",
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.w800,
+                                    color: Color(0xff0E2764)),
+                              ),
+                            ]),
+                      ),
+
+                ),
+
+                Column(
                   children: [
-                    Text(
-                      "소중한 질문을",
-                      style: TextStyle(
-                          fontFamily: "Pretendard",
-                          fontSize: 30,
-                          fontWeight: FontWeight.w700),
-                    ),
-                    Container(
-                      width: 340,
-                      height: 56,
-                      decoration: BoxDecoration(
-                          color: Color(0xffEBEFFF),
-                          border: Border.all(color: Color(0xffE6E6E6)),
-                          borderRadius: BorderRadius.circular(13)),
-                    ),
-                    Text(
-                      "남기다",
-                      style: TextStyle(
-                          fontFamily: "Pretendard",
-                          fontSize: 30,
-                          fontWeight: FontWeight.w700),
-                    )
+                    Expanded(child: Container(color: Colors.white)),
+                    SizedBox(height: 58),
+                    Expanded(child: Container(color: Colors.white)),
                   ],
                 ),
-              )
-            ],
-          ),
-          SlideTransition(
-              position: _offset,
-                child: Text(
-                  "학생이 학생에게",
-                  style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.w700,
-                      color: Color(0xff0E2764)
-                  ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    DearLogo(
+                      size: 50,
+                      color: Color(0xffD1D1D1),
+                    ),
+                    Padding(
+                        padding: EdgeInsets.symmetric(vertical: 40),
+                        child: Container(
+                          padding: EdgeInsets.only(bottom: 80),
+                          height: 240,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: const [
+                              Text(
+                                "소중한 질문을",
+                                style: TextStyle(
+                                    fontFamily: "Pretendard",
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.w700),
+                              ),
+                              Text(
+                                "남기다",
+                                style: TextStyle(
+                                    fontFamily: "Pretendard",
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.w700),
+                              )
+                            ],
+                          ),
+                        ))
+                  ],
                 )
-              )
-        ],
-      )
-    ));
+              ],
+            )));
   }
 }
