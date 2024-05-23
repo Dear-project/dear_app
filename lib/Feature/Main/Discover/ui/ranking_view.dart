ba06d387d56b7a40f767f67dc71e6746ff2ab4f8
jyw28 <roy03280328@gmail.com>
2024-05-23 20:01:04 +0900

    [Feature] :: Create 교수님 랭킹 뷰

--- lib/Feature/Main/Discover/ui/ranking_view.dart
+++ lib/Feature/Main/Discover/ui/ranking_view.dart
@@ -1,3 +1,4 @@
+import 'package:dear_app/Feature/Main/Discover/component/professor_ranking_cell.dart';
 import 'package:dear_app/Feature/Main/Shared/component/professor_cell.dart';
 import 'package:flutter/widgets.dart';
 
@@ -8,10 +9,30 @@ class RankingView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        SizedBox(height: 160),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 47),
+              child:
+              ProfessorRankingCell(RankingText: ProfessorRanking.second),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 47),
+              child:
+              ProfessorRankingCell(RankingText: ProfessorRanking.first),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 47),
+              child:
+              ProfessorRankingCell(RankingText: ProfessorRanking.third),
+            ),
+          ],
+        ),
+        SizedBox(height: 20),
         Center(
           child: SizedBox(
-            height: 400,
+            height: 352,
             child: SingleChildScrollView(
               child: Column(
                 children: [
