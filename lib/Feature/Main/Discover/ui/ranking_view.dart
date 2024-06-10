0e0bfc1b27e3487b0eb7988366ef2297d65690ac
jyw28 <roy03280328@gmail.com>
2024-06-10 18:11:59 +0900

    [Fix] :: Fix RankingView

--- lib/Feature/Main/Discover/ui/ranking_view.dart
+++ lib/Feature/Main/Discover/ui/ranking_view.dart
@@ -9,33 +9,33 @@ class RankingView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Padding(
-              padding: EdgeInsets.only(top: 47),
-              child:
-              ProfessorRankingCell(RankingText: ProfessorRanking.second),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 47),
-              child:
-              ProfessorRankingCell(RankingText: ProfessorRanking.first),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 47),
-              child:
-              ProfessorRankingCell(RankingText: ProfessorRanking.third),
-            ),
-          ],
-        ),
         SizedBox(height: 20),
         Center(
           child: SizedBox(
-            height: 352,
+            height: 677,
             child: SingleChildScrollView(
               child: Column(
                 children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: 47),
+                        child:
+                        ProfessorRankingCell(RankingText: ProfessorRanking.second),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 47),
+                        child:
+                        ProfessorRankingCell(RankingText: ProfessorRanking.first),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 47),
+                        child:
+                        ProfessorRankingCell(RankingText: ProfessorRanking.third),
+                      ),
+                    ],
+                  ),
                   for (int i = 0; i < 20; i++)
                     Padding(
                       padding: EdgeInsets.only(bottom: 14),
